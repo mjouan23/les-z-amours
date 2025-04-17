24376a7db5ab268b7a942af49dec913b440b0181
Magali JOUAN <jouan.magali@gmail.com>
2025-04-17 19:20:30 +0200

    Adaptation 30 ans de mariage Lydie&Dominique

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,38 +92,29 @@ document.addEventListener('keydown', function(event) {
         }
         video = document.createElement('video');
 
-        
         // Manche 1 Question 1 
         if (event.key === "1") {
             video.src = '../assets/video/Manche_1_Question_1_Encore un matin.webm';
         }
         // Manche 1 Question 2 
         else if (event.key === "2") {
-            video.src = '../assets/video/Manche_1_Question_2_clip_de_la_ferme_des_fatals picards.webm';
+            video.src = '../assets/video/Manche_1_Question_2_Zouk_Machine_Maldon.mp4';
         }
-        // Manche 1 Question 3 
+        // Manche 1 Question 3
         else if (event.key === "3") {
-            video.src = '../assets/video/Manche_1_Question_3_BA_CESAR_2016.mp4';
-        }
-        // Manche 1 Question 4
-        else if (event.key === "4") {
-            video.src = '../assets/video/Manche_1_Question_4_Joe_Cocker_You_Can_Leave_Your_Hat_On.mp4';
+            video.src = '../assets/video/Manche_1_Question_3_Joe_Cocker_You_Can_Leave_Your_Hat_On.mp4';
         } 
         // Manche 2 Question 1 
-        else if (event.key === "5") {
-            video.src = '../assets/video/Manche_2_Question_1_Boulevard_des_Airs_Emmène_moi.mp4';
+        else if (event.key === "4") {
+            video.src = '../assets/video/Manche_2_Question_1_Coup_De_Gueule.mp4';
         }
         // Manche 2 Question 2 
-        else if (event.key === "6") {
-            video.src = '../assets/video/Manche_2_Question_2_Zouk_Machine_Maldon.mp4';
+        if (event.key === "5") {
+            video.src = '../assets/video/Manche_2_Question_2_Movie_Company_Intros.mp4';
         }
         // Manche 2 Question 3 
-        if (event.key === "7") {
-            video.src = '../assets/video/Manche_2_Question_3_Movie_Company_Intros.mp4';
-        }
-        // Manche 2 Question 4 
-        else if (event.key === "8") {
-            video.src = '../assets/video/Manche_2_Question_4_ Joe_Cocker_You Can_Leave_Your_Hat_On.mp4';
+        else if (event.key === "6") {
+            video.src = '../assets/video/Manche_2_Question_3_ Joe_Cocker_You Can_Leave_Your_Hat_On.mp4';
         }
         video.controls = false; // Affiche les contrôles de la vidéo (play, pause, volume, etc.)
 
